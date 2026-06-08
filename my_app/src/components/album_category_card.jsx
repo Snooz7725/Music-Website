2e1d059f7b93cbf3d50122b91bcc879af1b74988
Snooze <220789576+Snooz7725@users.noreply.github.com>
2026-06-08 21:59:43 +0100

    feat(home): restyled interaction of cards in categories

--- my_app/src/components/album_category_card.jsx
+++ my_app/src/components/album_category_card.jsx
@@ -4,17 +4,35 @@ import './album_category_card.css'
 function CategoryCard({ artistId, albumId, albumTitle, artist, filePath }) {
     return (
         <li className="album-category-card-wrapper">
-            <Link to={`/album/${albumId}`}><div className="icon-wrapper">
-                <img className="thumbnail" src={'/assets/' + filePath} alt="Album cover" />
-                <img className="background-thumbnail" src={'/assets/' + filePath} alt="Album cover" />
-            </div></Link>
+            <Link to={`/album/${albumId}`}>
+                <div className="icon-wrapper">
+                    <img className="thumbnail" src={'/assets/' + filePath} alt="Album cover" />
+                    <img className="background-thumbnail" src={'/assets/' + filePath} alt="Album cover" />
+                </div>
+            </Link>
             <div className="details">
                 <Link to={`/album/${albumId}`}>
                     <h2>{albumTitle}</h2>
                 </Link>
+
                 <Link to={`/profile/${artistId}`}>
                     <h3>{`By | ${artist}`}</h3>
                 </Link>
+            </div>
+
+            <div className="clone-album-category-card-wrapper">
+                <Link to={`/album/${albumId}`}><div className="icon-wrapper">
+                    <img className="thumbnail" src={'/assets/' + filePath} alt="Album cover" />
+                    <img className="background-thumbnail" src={'/assets/' + filePath} alt="Album cover" />
+                </div></Link>
+                <div className="details">
+                    <Link to={`/album/${albumId}`}>
+                        <h2>{albumTitle}</h2>
+                    </Link>
+                    <Link to={`/profile/${artistId}`}>
+                        <h3>{`By | ${artist}`}</h3>
+                    </Link>
+                </div>
             </div>
         </li>
     )
--- my_app/src/components/song_category_card.jsx
+++ my_app/src/components/song_category_card.jsx
@@ -5,18 +5,42 @@ function SongCategoryCard({ artistId, songId, albumId, albumTitle, title, artist
     return (
         // TODO: ADD ROUTES
         <li className="song-category-card-wrapper">
-            <Link to={`/`}><div className="icon-wrapper">
-                <img className="thumbnail" src={'/assets/' + filePath} alt="Album cover" />
-                <img className="background-thumbnail" src={'/assets/' + filePath} alt="Album cover" />
-            </div></Link>
+            <Link to={`/`}>
+                <div className="icon-wrapper">
+                    <img className="thumbnail" src={'/assets/' + filePath} alt="Album cover" />
+                    <img className="background-thumbnail" src={'/assets/' + filePath} alt="Album cover" />
+                </div>
+            </Link>
             <div className="details">
                 <Link to={`/`}><h2>{title}</h2></Link>
+                
                 <Link to={`/album/${albumId}`}>
                     <h3>{`From | ${albumTitle}`}</h3>
                 </Link>
+
                 <Link to={`/profile/${artistId}`}>
                     <h3>{`By | ${artist}`}</h3>
                 </Link>
+            </div>
+
+            <div className="clone-song-category-card-wrapper">
+                <Link to={`/`}>
+                    <div className="icon-wrapper">
+                        <img className="thumbnail" src={'/assets/' + filePath} alt="Album cover" />
+                        <img className="background-thumbnail" src={'/assets/' + filePath} alt="Album cover" />
+                    </div>
+                </Link>
+                <div className="details">
+                    <Link to={`/`}><h2>{title}</h2></Link>
+                    
+                    <Link to={`/album/${albumId}`}>
+                        <h3>{`From | ${albumTitle}`}</h3>
+                    </Link>
+
+                    <Link to={`/profile/${artistId}`}>
+                        <h3>{`By | ${artist}`}</h3>
+                    </Link>
+                </div>
             </div>
         </li>
     )
