6c7c33b9b8318c2da3738235fa4629c1afc7e173
Snooze <jaden.k6202@gmail.com>
2026-06-22 15:50:00 +0100

    docs(server): adding comments

--- my_app/backend/server.js
+++ my_app/backend/server.js
@@ -9,14 +9,21 @@ import { readDb } from '../utils/db.js';
 const app = express();
 const PORT = 5000;
 
+// app.use(path = '/', middlewareFunction)
+// If no path is defined then it will default over to just using the 
+// middleware function for every request
 app.use(express.json());
 
+// After confirming that a path matches one of these, it matches the next part of the
+// path, such as looking at if '/albums' inside '/artists/albums' - this is done within
+// the respective files after the first match (in this case artistRoutes)
 app.use('/albums', albumRoutes);
 app.use('/artists', artistRoutes);
 app.use('/data', dataRoutes);
 app.use('/liked-albums', likedAlbumRoutes);
 app.use('/liked-songs', likedSongRoutes);
 
+// Starts the express server by allowing it to listen from a specified port
 app.listen(PORT, () => {
   readDb();
 
